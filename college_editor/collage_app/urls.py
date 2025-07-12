9e4940fa8a98464c1b37738844affb9a4e69f6d6
Sneha Vaish <126096173+sneha01vaish@users.noreply.github.com>
2025-07-12 23:36:59 +0530

    Update urls.py

--- college_editor/collage_app/urls.py
+++ college_editor/collage_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.collage_list, name='collage_list'),
+    path('', views.home, name='home'),
     path('upload/', views.upload_images, name='upload_images'),
     # path('upload_collage/', views.upload_collage, name='upload_collage'),
     path('select/', views.select_images, name='select_images'),
